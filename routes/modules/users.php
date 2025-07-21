66684447a5e02edb59bfc06cdd0c3a2a170ca742
Helen Marques <byhelenmedeiros@gmail.com>
2025-07-21 19:35:58 +0100

    Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');

--- routes/modules/users.php
+++ routes/modules/users.php
@@ -3,3 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\UserController;
 Route::get("/", [UserController::class,"index"])->name("admin.users.index");
+Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
